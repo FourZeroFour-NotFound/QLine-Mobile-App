6d6be9a6d64a1e1cfabcee065f226c581666c4c9
esraazaitoun1991 <eng.esraaz1991@gmail.com>
2019-01-11 18:22:47 +0200

    (update) updating mobile app

---
+++ Android/IOS/database/schema.sql
@@ -0,0 +1,343 @@
+<<<<<<< HEAD
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'user'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `user`;
+		
+CREATE TABLE `user` (
+  `user_id` INTEGER(20) NOT NULL AUTO_INCREMENT,
+  `firstName` VARCHAR(25) NOT NULL,
+  `lastName` VARCHAR(25) NOT NULL,
+  `email` VARCHAR(25) NOT NULL,
+  `password` VARCHAR(25) NOT NULL,
+  `organization` VARCHAR(25) NOT NULL,
+  `phoneNumber` INTEGER(25) NOT NULL,
+  `primum` INTEGER(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`user_id`, `organization`, `email`)
+);
+
+-- ---
+-- Table 'queue'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `queue`;
+		
+CREATE TABLE `queue` (
+  `queue_id` INTEGER NOT NULL AUTO_INCREMENT,
+  `nameOfQueeu` VARCHAR(25) NOT NULL,
+  `start_time` TIME(6) NOT NULL,
+  `end_time` TIME(6) NOT NULL,
+  `date` DATE NOT NULL,
+  `timeforone` VARCHAR(25) NOT NULL,
+  `windows` VARCHAR(25) NOT NULL,
+  `imgUrl` VARCHAR(200) NOT NULL,
+  `take_premum` INTEGER(1) NOT NULL DEFAULT 0,
+  `accept_join` INTEGER(1) NOT NULL DEFAULT 0,
+  `requierment` VARCHAR(80) NOT NULL,
+  `creator_id` INTEGER(20) NOT NULL,
+  PRIMARY KEY (`queue_id`)
+);
+
+-- ---
+-- Table 'user_queue'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `user_queue`;
+		
+CREATE TABLE `user_queue` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `user_id` INTEGER(20) NOT NULL,
+  `queue_id` INTEGER NOT NULL,
+  `onwindow` INTEGER(25) NOT NULL DEFAULT 0,
+  `Notes` VARCHAR(200) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'waitingList'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `waitingList`;
+		
+CREATE TABLE `waitingList` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `user_id` INTEGER(20) NOT NULL,
+  `queue_id` INTEGER NOT NULL,
+  `onwindow` SMALLINT(25) NOT NULL DEFAULT 0,
+  `notes` VARCHAR(200) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'history'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `history`;
+		
+CREATE TABLE `history` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `queu_name` VARCHAR(25) NOT NULL,
+  `organization` VARCHAR(25) NOT NULL,
+  `date` DATE NOT NULL,
+  `numOfTicket` INTEGER NOT NULL,
+  `creator_id` INTEGER(20) NOT NULL,
+  `start_time` TIME NOT NULL,
+  `end_time` TIME NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'customer'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `customer`;
+		
+CREATE TABLE `customer` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(250) NOT NULL,
+  `email` VARCHAR(250) NOT NULL,
+  `phoneNumber` VARCHAR(20) NOT NULL,
+  `comments` VARCHAR(250) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'customerchat'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `customerchat`;
+		
+CREATE TABLE `customerchat` (
+  `id` INTEGER(20) NOT NULL,
+  `message` VARCHAR(200) NOT NULL
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `queue` ADD FOREIGN KEY (creator_id) REFERENCES `user` (`user_id`);
+ALTER TABLE `user_queue` ADD FOREIGN KEY (user_id) REFERENCES `user` (`user_id`);
+ALTER TABLE `user_queue` ADD FOREIGN KEY (queue_id) REFERENCES `queue` (`queue_id`);
+ALTER TABLE `waitingList` ADD FOREIGN KEY (user_id) REFERENCES `user` (`user_id`);
+ALTER TABLE `waitingList` ADD FOREIGN KEY (queue_id) REFERENCES `queue` (`queue_id`);
+ALTER TABLE `history` ADD FOREIGN KEY (creator_id) REFERENCES `user` (`user_id`);
+ALTER TABLE `customer` ADD FOREIGN KEY (id) REFERENCES `user` (`user_id`);
+ALTER TABLE `customerchat` ADD FOREIGN KEY (id) REFERENCES `user` (`user_id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `user` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `queue` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `user_queue` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `waitingList` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `history` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `customer` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `customerchat` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `user` (`user_id`,`firstName`,`lastName`,`email`,`password`,`organization`,`phoneNumber`,`primum`) VALUES
+-- ('','','','','','','','');
+-- INSERT INTO `queue` (`queue_id`,`nameOfQueeu`,`start_time`,`end_time`,`date`,`timeforone`,`windows`,`imgUrl`,`take_premum`,`accept_join`,`requierment`,`creator_id`) VALUES
+-- ('','','','','','','','','','','','');
+-- INSERT INTO `user_queue` (`id`,`user_id`,`queue_id`,`onwindow`,`Notes`) VALUES
+-- ('','','','','');
+-- INSERT INTO `waitingList` (`id`,`user_id`,`queue_id`,`onwindow`,`notes`) VALUES
+-- ('','','','','');
+-- INSERT INTO `history` (`id`,`queu_name`,`organization`,`date`,`numOfTicket`,`creator_id`,`start_time`,`end_time`) VALUES
+-- ('','','','','','','','');
+-- INSERT INTO `customer` (`id`,`name`,`email`,`phoneNumber`,`comments`) VALUES
+-- ('','','','','');
+-- INSERT INTO `customerchat` (`id`,`message`) VALUES
+=======
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'user'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `user`;
+		
+CREATE TABLE `user` (
+  `user_id` INTEGER(20) NOT NULL AUTO_INCREMENT,
+  `firstName` VARCHAR(25) NOT NULL,
+  `lastName` VARCHAR(25) NOT NULL,
+  `email` VARCHAR(25) NOT NULL,
+  `password` VARCHAR(25) NOT NULL,
+  `organization` VARCHAR(25) NOT NULL,
+  `phoneNumber` INTEGER(25) NOT NULL,
+  `primum` INTEGER(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`user_id`, `organization`, `email`)
+);
+
+-- ---
+-- Table 'queue'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `queue`;
+		
+CREATE TABLE `queue` (
+  `queue_id` INTEGER NOT NULL AUTO_INCREMENT,
+  `nameOfQueeu` VARCHAR(25) NOT NULL,
+  `start_time` TIME(6) NOT NULL,
+  `end_time` TIME(6) NOT NULL,
+  `date` DATE NOT NULL,
+  `timeforone` VARCHAR(25) NOT NULL,
+  `windows` VARCHAR(25) NOT NULL,
+  `imgUrl` VARCHAR(200) NOT NULL,
+  `take_premum` INTEGER(1) NOT NULL DEFAULT 0,
+  `accept_join` INTEGER(1) NOT NULL DEFAULT 0,
+  `requierment` VARCHAR(80) NOT NULL,
+  `creator_id` INTEGER(20) NOT NULL,
+  PRIMARY KEY (`queue_id`)
+);
+
+-- ---
+-- Table 'user_queue'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `user_queue`;
+		
+CREATE TABLE `user_queue` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `user_id` INTEGER(20) NOT NULL,
+  `queue_id` INTEGER NOT NULL,
+  `onwindow` INTEGER(25) NOT NULL DEFAULT 0,
+  `Notes` VARCHAR(200) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'waitingList'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `waitingList`;
+		
+CREATE TABLE `waitingList` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `user_id` INTEGER(20) NOT NULL,
+  `queue_id` INTEGER NOT NULL,
+  `onwindow` SMALLINT(25) NOT NULL DEFAULT 0,
+  `notes` VARCHAR(200) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'history'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `history`;
+		
+CREATE TABLE `history` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `queu_name` VARCHAR(25) NOT NULL,
+  `organization` VARCHAR(25) NOT NULL,
+  `date` DATE NOT NULL,
+  `numOfTicket` INTEGER NOT NULL,
+  `creator_id` INTEGER(20) NOT NULL,
+  `start_time` TIME NOT NULL,
+  `end_time` TIME NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'customer'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `customer`;
+		
+CREATE TABLE `customer` (
+  `id` INTEGER(20) NOT NULL,
+  `name` VARCHAR(250) NOT NULL,
+  `email` VARCHAR(250) NOT NULL,
+  `phoneNumber` VARCHAR(20) NOT NULL,
+  `comments` VARCHAR(250) NULL DEFAULT NULL
+);
+
+-- ---
+-- Table 'customerchat'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `customerchat`;
+		
+CREATE TABLE `customerchat` (
+  `id` INTEGER(20) NOT NULL,
+  `message` VARCHAR(200) NOT NULL
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `queue` ADD FOREIGN KEY (creator_id) REFERENCES `user` (`user_id`);
+ALTER TABLE `user_queue` ADD FOREIGN KEY (user_id) REFERENCES `user` (`user_id`);
+ALTER TABLE `user_queue` ADD FOREIGN KEY (queue_id) REFERENCES `queue` (`queue_id`);
+ALTER TABLE `waitingList` ADD FOREIGN KEY (user_id) REFERENCES `user` (`user_id`);
+ALTER TABLE `waitingList` ADD FOREIGN KEY (queue_id) REFERENCES `queue` (`queue_id`);
+ALTER TABLE `history` ADD FOREIGN KEY (creator_id) REFERENCES `user` (`user_id`);
+ALTER TABLE `customer` ADD FOREIGN KEY (id) REFERENCES `user` (`user_id`);
+ALTER TABLE `customerchat` ADD FOREIGN KEY (id) REFERENCES `user` (`user_id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `user` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `queue` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `user_queue` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `waitingList` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `history` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `customer` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `customerchat` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `user` (`user_id`,`firstName`,`lastName`,`email`,`password`,`organization`,`phoneNumber`,`primum`) VALUES
+-- ('','','','','','','','');
+-- INSERT INTO `queue` (`queue_id`,`nameOfQueeu`,`start_time`,`end_time`,`date`,`timeforone`,`windows`,`imgUrl`,`take_premum`,`accept_join`,`requierment`,`creator_id`) VALUES
+-- ('','','','','','','','','','','','');
+-- INSERT INTO `user_queue` (`id`,`user_id`,`queue_id`,`onwindow`,`Notes`) VALUES
+-- ('','','','','');
+-- INSERT INTO `waitingList` (`id`,`user_id`,`queue_id`,`onwindow`,`notes`) VALUES
+-- ('','','','','');
+-- INSERT INTO `history` (`id`,`queu_name`,`organization`,`date`,`numOfTicket`,`creator_id`,`start_time`,`end_time`) VALUES
+-- ('','','','','','','','');
+-- INSERT INTO `customer` (`id`,`name`,`email`,`phoneNumber`,`comments`) VALUES
+-- ('','','','','');
+-- INSERT INTO `customerchat` (`id`,`message`) VALUES
+>>>>>>> 544fca0d44dac5fa195ee45ca790b1bf82bd0e86
+-- ('','');
--- Android/IOS/screens/search.js
+++ Android/IOS/screens/search.js
@@ -13,26 +13,41 @@ import {
   Button,
   Alert,
   TouchableHighlight,
+  AsyncStorage
 } from 'react-native';
 import { SearchBar } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome';
 import { WebBrowser } from 'expo';
 import { MonoText } from '../components/StyledText';
 
 export default class Search extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: '' };
+    this.state = { 
+      user_id : "zait"
+     };
   }
   static navigationOptions = {
     header: null,
   };
+  test = function (){
+    storeToken = async accessToken => {
+      try {
+        await AsyncStorage.setItem("userID", JSON.stringify(accessToken));
+      } catch (error) {
+        console.warn("storeToken error:", error);
+      }
+    };
+  }
+  
+
 
   render() {
     const inputAccessoryViewID = "uniqueID";
+    // const value = await AsyncStorage.getItem("userID")
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+        <View><Text>{AsyncStorage.getItem("userID").userID}</Text></View>
           <View style={styles.welcomeContainer}>
             <Image
               source={{ uri: 'https://i.ibb.co/SN5qmhF/qline.png' }}
@@ -40,7 +55,25 @@ export default class Search extends React.Component {
             />
           </View>
           <View>
-            <Text style={{ marginLeft: 50, fontSize: 18 }}>Joine to the queue by queue ID:</Text>
+          <Text style={{ marginLeft: 50, fontSize: 18 }}>{console.log(this.state.user_id)}Enter your ID to enjoy our service</Text>
+          </View>
+          <View style={{ marginTop: 20 }}>
+          <TextInput
+              style={{ alignItems: 'center', backgroundColor: 'white' }}
+              placeholder='Your ID'
+              onChange={(e)=>{console.log('e is :',e)}}
+            />
+          </View>
+          <View style={{ marginTop: 20, marginLeft: 120, height: 50, width: 100  }}>
+                <Button
+                  onPress={this.test}
+                  title="Enter"
+                  color="#aa1256"
+                />
+          </View>
+          
+          <View>
+            <Text style={{ marginLeft: 50, fontSize: 18 }}>Join to the queue by queue ID:</Text>
           </View>
           <View style={{ marginTop: 20 }}>
             <TextInput
@@ -57,18 +90,17 @@ export default class Search extends React.Component {
             style={{ alignItems: 'center', justifyContent: 'center' }} 
             // onPress={() => { this._fetchResults() }} 
             underlayColor='transparent'>
-              <View>
+              <View style={{ marginTop: 20 }}>
                 <Button
                   onPress={() => console.log('Ask me later pressed')}
                   title="Search"
                   color="#aa1256"
                 />
-                {/* <Icon name="search" size='20' color="#4285F4" /> */}
               </View>
             </TouchableHighlight>
           </View>
           <Text style={{ marginTop: 40, marginLeft: 70, fontSize: 18 }}>Scan the barcode for the queue:</Text>
-          <View style={{ marginTop: 20, marginLeft: 140, height: 50, width: 100 }}>
+          <View style={{ marginTop: 20, marginLeft: 130, height: 50, width: 100 }}>
             <Button
               onPress={() => console.log('Ask me later pressed')}
               title="Scan"
